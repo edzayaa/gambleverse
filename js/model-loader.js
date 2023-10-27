14a8b3b0d5b321c1bb07d1bd35fa758156d5f2d7
Brandon Martinez <brandonamv@gmail.com>
2023-10-27 12:19:25 -0400

    update model corrections

--- js/model-loader.js
+++ js/model-loader.js
@@ -1,6 +1,7 @@
 
 var active1=false,active2=false,active3=false;
 var scrollDir,scrollAct=window.scrollY;
+var scrollActive=false;
 const observer = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
 	  if (entry.isIntersecting) {
@@ -45,8 +46,12 @@ document.addEventListener('scroll',()=>{
 	}else{
 		scrollDir=-1
 	}
+	scrollActive=true;
 	scrollAct=window.scrollY;
 });
+document.addEventListener('scrollend',()=>{
+	scrollActive=false;
+})
 const canvas1 = document.getElementById("model_1"); // Get the canvas element
 const engine1 = new BABYLON.Engine(canvas1, true); // Generate the BABYLON 3D engine
 const createScene1 = function () {
@@ -89,13 +94,13 @@ const createScene1 = function () {
 
 	
 	// Append glTF model to scene.
-	BABYLON.SceneLoader.Append("3D/", "model_2.glb", scene, function (mesh) {
+	BABYLON.SceneLoader.Append("3D/", "model_1.glb", scene, function (mesh) {
 		// Create a default arc rotate camera and light.
 		mesh.createDefaultCamera(true, true, true);
 	
 		// The default camera looks at the back of the asset.
 		// Rotate the camera by 180 degrees to the front of the asset.
-		mesh.activeCamera.alpha += Math.PI*1.01;
+		mesh.activeCamera.alpha += Math.PI*1.1;
 	
 		//scaling
 		mesh.meshes[0].scaling = new BABYLON.Vector3(1.15, 1.15, 1.15);
@@ -142,7 +147,7 @@ engine1.runRenderLoop(function () {
 	
 	if (active1) {
 		scene1.render();
-		if (scene1.meshes.length>0) {
+		if (scene1.meshes.length>0 && scrollActive) {
 			scene1.meshes[0].rotation.y += 0.002 *scrollDir;
 			scene1.meshes[1].rotation.y += 0.002*scrollDir;
 			scene1.meshes[2].rotation.y += 0.002*scrollDir;
@@ -193,13 +198,13 @@ const createScene2 = function () {
 	pointLight2.diffuse = new BABYLON.Color3(10, 10, 255);
 		// Append glTF model to scene.
 
-	BABYLON.SceneLoader.Append("3D/", "model_1.glb", scene, function (sceneMesh) {
+	BABYLON.SceneLoader.Append("3D/", "model_2.glb", scene, function (sceneMesh) {
 		// Create a default arc rotate camera and light.
 		sceneMesh.createDefaultCamera(true, true, true);
 	
 		// The default camera looks at the back of the asset.
 		// Rotate the camera by 180 degrees to the front of the asset.
-		sceneMesh.activeCamera.alpha += Math.PI;
+		sceneMesh.activeCamera.alpha += Math.PI*0.7;
 	
 		//scaling
 		sceneMesh.meshes[0].scaling = new BABYLON.Vector3(1.2, 1.2, 1.2);
@@ -257,7 +262,7 @@ engine2.runRenderLoop(function () {
 	
 	if (active2) {
 		scene2.render();
-		if (scene2.meshes.length>0) {
+		if (scene2.meshes.length>0 && scrollActive) {
 			scene2.meshes[0].rotation.y += 0.002 *scrollDir;
 			scene2.meshes[1].rotation.y += 0.002*scrollDir;
 			scene2.meshes[2].rotation.y += 0.002*scrollDir;
@@ -287,7 +292,31 @@ const createScene3 = function () {
 	// Dim the light a small amount - 0 to 1
 	light.intensity = 0.7;
 	// Append glTF model to scene. */
+	// Creates a light, aiming 0,1,0 - to the sky
+	const light = new BABYLON.HemisphericLight("light", 
+	new BABYLON.Vector3(0, 1, 0), scene);
+	// // Dim the light a small amount - 0 to 1
+	light.intensity = 0.3;
+	// Append glTF model to scene.
 
+	// Creates a PointLight, aiming 0,1,0 - to the sky
+	var pointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(5, 70, 25), scene);
+	// // Dim the light a small amount - 0 to 1
+	pointLight.intensity = 100;
+	pointLight.radius = 10
+
+	// Change the color of the PointLight
+	pointLight.diffuse = new BABYLON.Color3(106, 94, 209);
+	//pointLight.specular = new BABYLON.Color3(87, 35, 100);
+	// Creates a PointLight, aiming 0,1,0 - to the sky
+	var pointLight2 = new BABYLON.PointLight("pointLight2", new BABYLON.Vector3(-10, 85, 45), scene);
+	// // Dim the light a small amount - 0 to 1
+	pointLight2.intensity = 240;
+	pointLight2.radius = 10
+
+	// Change the color of the PointLight
+	pointLight2.diffuse = new BABYLON.Color3(10, 10, 255);
+	// Append glTF model to scene.
 	// Creates a light, aiming 0,1,0 - to the sky
 	var pointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0, 1, 0), scene);
 	// // Dim the light a small amount - 0 to 1
